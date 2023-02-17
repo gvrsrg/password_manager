5dfe2d09622055f9dde5ae75d11979d9f1e90496
gvrsrg <gvrsrg@gmail.com>
2023-02-17 03:03:55 +0200

    added search

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ from random import choice, randint, shuffle
 import pyperclip
 import json
 
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
-#Password Generator Project
+# Password Generator Project
+
 def generate_password():
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+               'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+               'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
@@ -27,7 +31,6 @@ def generate_password():
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
-
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
@@ -41,18 +44,44 @@ def save():
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
     else:
-        with open("data.json", "r") as data_file:
-            data = json.load(data_file)
+        try:
+            with open("data.json", "r") as data_file:
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(dict_to_save, data_file, indent=4)
+        else:
             data.update(dict_to_save)
-
-        with open("data.json", "w") as data_file:
-            json.dump(databbb, data_file, indent=4)
-
+            with open("data.json", "w") as data_file:
+                json.dump(data, data_file, indent=4)
+        finally:
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 
 
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+def search():
+    website = website_entry.get()
+
+    try:
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo("Error", "Datafile not found!")
+    else:
+        if website in data:
+            pwddata = data[website]
+            email = pwddata["email"]
+            password = pwddata["password"]
+            messagebox.showinfo(website, f"Password is {password}\nEmail is {email}")
+        else:
+            messagebox.showinfo("Error", f"Password for the site {website} not found")
+
+
+
 # ---------------------------- UI SETUP ------------------------------- #
+
 
 window = Tk()
 window.title("Password Manager")
@@ -63,7 +92,7 @@ logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=0, column=1)
 
-#Labels
+# Labels
 website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
 email_label = Label(text="Email/Username:")
@@ -71,7 +100,7 @@ email_label.grid(row=2, column=0)
 password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
-#Entries
+# Entries
 website_entry = Entry(width=35)
 website_entry.grid(row=1, column=1, columnspan=1, sticky="we")
 website_entry.focus()
@@ -82,7 +111,7 @@ password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1, sticky="we")
 
 # Buttons
-search_button = Button(text="Search", width=36, command=save)
+search_button = Button(text="Search", width=36, command=search)
 search_button.grid(row=1, column=2, columnspan=1)
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2, sticky="we")
